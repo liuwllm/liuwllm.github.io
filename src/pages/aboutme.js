f14edf0877e5da405a3553ff7712a81892215160
liuwllm <liu.wllm@gmail.com>
2023-02-09 00:20:38 -0500

    Fix font weighting issues

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -16,7 +16,7 @@ export default function About() {
             <link rel="icon" href="/favicon.ico" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
         </Head>
         <main className="bg-slate-200 min-h-screen min-w-fit font-sans">
             <HeaderComponent />
